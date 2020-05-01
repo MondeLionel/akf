fa164a232b22e2e59d3be1a78861daf5afeac86b
Monde Nyawose <lionelnyawose@gmail.com>
2020-05-01 23:20:08 +0200

    update css

--- js/main.js
+++ js/main.js
@@ -2,11 +2,26 @@
 
 	var mySwiper = new Swiper ('.swiper-container', {
 		direction: 'horizontal',
+		 keyboard: {
+        	enabled: true,
+      	},
+      	autoplay: {
+        	delay: 25000,
+        	disableOnInteraction: false,
+      },
 		navigation: {
-			nextEl: '.swiper-button-next',
+			nextEl: '.next',
 			prevEl: '.swiper-button-prev',
 		},
 		scrollbar: {
 			el: '.swiper-scrollbar',
 		},
 	});
+
+	var nextButton = document.getElementsByClassName('next');
+	var _nextFunc = function(){
+		mySwiper.SlideNext();
+
+	}
+
+	nextButton[0].addEventListener("click",_nextFunc);
