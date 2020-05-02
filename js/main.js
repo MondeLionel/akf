e2163e7643b41f96d6ee662a9deab999ccaa4fc6
Monde Nyawose <lionelnyawose@gmail.com>
2020-05-02 20:17:39 +0200

    update menu items and typography

--- js/main.js
+++ js/main.js
@@ -25,3 +25,42 @@
 	}
 
 	nextButton[0].addEventListener("click",_nextFunc);
+
+// Shitty js dev work
+
+var body = document.getElementsByTagName("body")[0];
+
+function openWork(){
+	body.classList.add("work");
+	body.classList.remove("contact")
+	body.classList.remove("about")
+}
+
+function openContact(){
+	body.classList.add("contact")
+	body.classList.remove("work")
+	body.classList.remove("about")
+}
+
+function openAbout(){
+	body.classList.add("about")
+	body.classList.remove("work")
+	body.classList.remove("contact")
+
+}
+
+
+function removeBodyClasses(){
+	body.classList.remove("work")
+	body.classList.remove("contact")
+	body.classList.remove("about")
+}
+
+
+
+
+
+
+
+
+
